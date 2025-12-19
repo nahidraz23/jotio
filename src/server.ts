270e78287d0702a023dd5e930c4b56898cc375c7
nahidraz23 <nzamanraz@gmail.com>
2025-12-19 16:49:57 +0600

    fix: secret key secured using dotenv

--- src/server.ts
+++ src/server.ts
@@ -1,20 +1,23 @@
-import {Server} from 'http';
-import app from './app';
-import mongoose from 'mongoose';
+import { Server } from "http";
+import app from "./app";
+import mongoose from "mongoose";
+import "dotenv/config";
 
 let server: Server;
 const port = 5000;
+const dbUser = process.env.DB_USER;
+const dbPass = process.env.DB_PASS;
 
-async function main () {
-    try {
-        await mongoose.connect("mongodb+srv://nzamanraz:GSyfYJr6ufUNvcK6@cluster0.yzsgyep.mongodb.net/?appName=Cluster0");
-        console.log("Connected to mongoDB using moongose");
-        server = app.listen(port, () => {
-            console.log(`Server is running on port: ${port}`);
-        })
-    } catch (error) {
-        
-    }
+async function main() {
+  try {
+    await mongoose.connect(
+      `mongodb+srv://${dbUser}:${dbPass}@cluster0.yzsgyep.mongodb.net/?appName=Cluster0`
+    );
+    console.log("Connected to mongoDB using moongose");
+    server = app.listen(port, () => {
+      console.log(`Server is running on port: ${port}`);
+    });
+  } catch (error) {}
 }
 
-main()
+main();
