9974c764b9037b2eff01e609097b5b4d0d5a661f
nahidraz23 <nzamanraz@gmail.com>
2025-12-22 00:54:14 +0600

    add: multiple approach for create note and get notes

--- src/app.ts
+++ src/app.ts
@@ -3,6 +3,8 @@ import { model, Schema } from 'mongoose';
 
 const app: Application = express();
 
+app.use(express.json())
+
 const noteSchema = new Schema({
     // title: String,
     title: {type: String, required: true, trim: true}, // trim will remove extra white spaces "           Hello World        "
@@ -17,26 +19,64 @@ const noteSchema = new Schema({
     pinned: {
         type: Boolean,
         default: false
+    },
+    tags: {
+        label: {type: String, required: true},
+        color: {type: String, default: 'gray'}
     }
 })
 
 const Note = model('Note', noteSchema)
 
-app.post('/create-note', async (req: Request, res: Response) => {
-    const myNote = new Note ({
-        title: 'Learning Mongoose',
-        // content: 'I am learning Mongoose',
-        // content: true,
-        // publishDate: 'Hello World'
-        
-    })
+app.get('/notes', async (req: Request, res: Response) => {
+    const notes = await Note.find();
 
-    await myNote.save();
+    res.status(201).json({
+        success: true,
+        message: "Notes found!!!",
+        notes
+    })
+})
+
+app.get('/notes/:noteId', async (req: Request, res: Response) => {
+    const noteId = req.params.noteId;
+
+    // approach-1 for finding single note
+    // const note = await Note.findById(noteId);
+
+    // approach-2 for finding single note
+    const note = await Note.findOne({_id : noteId});
+
+    res.status(201).json({
+        success: true,
+        message: "Notes found!!!",
+        note
+    })
+})
+
+app.post('/notes/create-note', async (req: Request, res: Response) => {
+    
+    const body = req.body;
+
+    // approach-1 of creating note
+    // const myNote = new Note ({
+    //     // title: 'Learning Mongoose',
+    //     // content: 'I am learning Mongoose',
+    //     // content: true,
+    //     // publishDate: 'Hello World'
+        
+    // })
+
+    // approach-2
+    const note = await Note.create(body)
+
+    // await myNote.save();
+
 
     res.status(201).json({
         success: true,
         message: 'Note create successfully',
-        note: myNote
+        note
     })
 })
 
