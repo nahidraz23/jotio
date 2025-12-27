eebf23a5869b2cf5e6ce75accc3630fdbb608fda
nahidraz23 <nzamanraz@gmail.com>
2025-12-27 14:53:32 +0600

    feat: custom validation added for email

--- src/app/models/user.model.ts
+++ src/app/models/user.model.ts
@@ -3,14 +3,51 @@ import { IUser } from '../interfaces/user.interface'
 
 const userSchema = new Schema<IUser>(
     {
-        email: { type: String, required: true, lowercase: true, trim: true, unique: true},
-        firstName: { type: String, required: true, trim: true, minLength: 5, maxLength: 10 },
-        lastName: { type: String, required: true, trim: true, minLength: [5, 'Last name should be minimum 5 charecters long'], maxLength: 10  },
-        age: { type: Number, required: true, min: [18, 'Age must be 18, got {VALUE}'], max: 60 },
-        password: { type: String, required: true },
+        email: { 
+            type: String, 
+            required: true, 
+            lowercase: true, 
+            trim: true, 
+            unique: true,
+            validate: {
+                validator: function (value) {
+                    return /^[^\s@]+@[^\s@]+\.[^\s@]+$/.test(value)
+                },
+                message: function (props) {
+                    return `${props.value} is not a valid email`
+                }
+            }
+        },
+        firstName: { 
+            type: String, 
+            required: true, 
+            trim: true, 
+            minLength: 5, 
+            maxLength: 10 
+        },
+        lastName: { 
+            type: String, 
+            required: true, 
+            trim: true, 
+            minLength: [5, 'Last name should be minimum 5 charecters long'], 
+            maxLength: 10  
+        },
+        age: { 
+            type: Number, 
+            required: true, 
+            min: [18, 'Age must be 18, got {VALUE}'], 
+            max: 60 
+        },
+        password: { 
+            type: String, 
+            required: true 
+        },
         role: {
             type: String,
-            enum: ['USER', 'ADMIN', 'SUPERADMIN'],
+            enum: {
+                values: ["USER", "ADMIN", "SUPERADMIN"],
+                message: "Role is not valid"
+            },
             uppercase: true,
             default: 'USER'
         }
